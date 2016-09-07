dd4ba94d7d11bc98f6481da7c76de325153dd3e3
Dago Israel <dagoisrael@gmail.com>
2016-09-07 10:54:42 +0300

    loginFrame done

--- src/client/guiControllers/LoginFrameController.java
+++ src/client/guiControllers/LoginFrameController.java
@@ -6,12 +6,15 @@
 package client.guiControllers;
 
 import client.Client;
+import client.servicesController.ServicesController;
+import dto.AdminDTO;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -57,6 +60,34 @@ public class LoginFrameController implements Initializable {
     @FXML
     private void onRegisterBtnClick(ActionEvent e) throws IOException{
         this.client.showRegisterFrame(e);
+    }
+    
+    @FXML
+    private void onLoginBtnClick(ActionEvent e) throws IOException{
+        try {
+            String user = userFld.getText();
+            String parola = parolaFld.getText();
+            AdminDTO rez = ServicesController.getInstance().login(user, parola);
+            if(rez!=null){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setResizable(false);
+                alert.setContentText("Login reusit");
+                alert.showAndWait();                     
+            }else{
+                Alert alert = new Alert(Alert.AlertType.WARNING); 
+                alert.setHeaderText("Login Failed");
+                alert.setResizable(false);
+                alert.setContentText("you maybe wrong your username or your password...try after double check");
+                alert.showAndWait();
+            }
+        } catch (Exception ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR); 
+                alert.setTitle("");
+                alert.setResizable(false);
+                alert.setContentText("Somes errors happened during your request");
+                alert.showAndWait();
+        }
+        userFld.setText(""); parolaFld.setText("");        
     }
     
     
--- src/client/guiControllers/RegisterFrameController.java
+++ src/client/guiControllers/RegisterFrameController.java
@@ -12,18 +12,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 /**
- * FXML Controller class
- *
- * @author Israel Dago
+ * FXML Controller clgo
  */
 public class RegisterFrameController implements Initializable {
     
